dd77c800df58fc9cfbbaad1d915d291e897a8566
Aurora Meilian Karlsen <auroramk@stud.ntnu.no>
2025-01-03 15:12:14 +0100

    files for chapter 3

---
+++ notes/chapter3.js
@@ -0,0 +1,9 @@
+//data manipulation 
+
+let exampleScentence = "This is s atring"
+
+let stingLength = exampleScentence.length
+
+let combinedString = "hello world" + " " + "my name is Aurora" + "fifth letter of example string is:" + exampleScentence[4]
+
+console.log(combinedString)
